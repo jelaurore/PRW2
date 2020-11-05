602c509e7fc12a95fa0aa668af4e6aa7390cdd0e
jelaurore <jeanlauroree@gmail.com>
2020-11-05 03:49:01 -0500

    Done wiht the navigation menu

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,12 +1,12 @@
 import React, {useState} from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 // import { Link } from 'react-router-dom';
-// //Icons
-// import { VscHome } from 'react-icons/vsc';
-// import { BiMoon } from 'react-icons/bi';
-// import { SiCircle } from 'react-icons/si';
-// import { CgProfile } from 'react-icons/cg';
-// import { AiOutlineContacts } from 'react-icons/ai';
+//Icons
+import { VscHome } from 'react-icons/vsc';
+import { BiMoon } from 'react-icons/bi';
+import { SiCircle } from 'react-icons/si';
+import { CgProfile } from 'react-icons/cg';
+import { AiOutlineContacts } from 'react-icons/ai';
 
 function Nav() {
   const [open, setOpen] = useState(false);
@@ -16,12 +16,12 @@ function Nav() {
     <nav>
       <div className="logo">Peace</div>
       <ul className="nav-links"
-      style={{transform: open ? "translateX(0px)" : "translateX (-500px)"}}>
-        <li><a>Home</a></li>
-        <li><a>Sleep</a></li>
-        <li><a>Meditate</a></li>
-        <li><a>Profile</a></li>
-        <li><a>Contact</a></li>
+      style={{transform: open ? "translateX(0px)" : ""}}>
+        <li><a href="#home"><VscHome /></a></li>
+        <li><a href="#sleep"><BiMoon /></a></li>
+        <li><a href="#meditate"><SiCircle /></a></li>
+        <li><a href="#profile"><CgProfile /></a></li>
+        <li><a href="#contact"><AiOutlineContacts /></a></li>
       </ul>
       <i onClick={()=>setOpen(!open)} className="burger-menu"><GiHamburgerMenu/></i>
       {/* <Link to='#'><VscHome /></Link>
