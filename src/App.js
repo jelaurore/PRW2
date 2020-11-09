48cb6816b3c49ebb6c919f4add5835db35eed2d8
jelaurore <jeanlauroree@gmail.com>
2020-11-09 11:04:01 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React, {Component} from 'react';
 import './App.css';
-// import Nav from './component/Nav'; 
-// import Contact from './pages/Contact';
 import { BrowserRouter as Router} from 'react-router-dom';
 import Routes from './component/Routes';
-// import Home from './pages/Home';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <Routes />
-        {/* <div>
-          <Nav />
-          <Route path='/home' component={Home} />
-          <Route path='/contact' component={Contact} />
-        </div> */}
       </Router>
     );
   }
